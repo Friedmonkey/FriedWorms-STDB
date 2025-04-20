8ba6615dfb4aa0b8c458768ccb8bf4bc017c28c8
FriedMonkey <120042050+Friedmonkey@users.noreply.github.com>
2025-04-20 02:04:53 +0200

    Add project files.

---
+++ FriedWorms.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+using SpacetimeDB;
+using SpacetimeDB.ClientApi;
+
+
+namespace FriedWorms;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //SpacetimeDB.BSATN.Bool
+        InitWindow(800, 480, "Hello World");
+
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(Color.White);
+
+            DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            EndDrawing();
+        }
+
+        CloseWindow();
+        //Console.WriteLine("Hello, World!");
+    }
+}
